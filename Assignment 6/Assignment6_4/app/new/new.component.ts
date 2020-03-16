79185caf3fa803bb6b495b89798dddd780786d67
Sanket Wadekar <46665854+sanketwadekar3@users.noreply.github.com>
2020-03-16 12:45:35 +0530

    Add files via upload

---
+++ Assignment 6/Assignment6_4/app/new/new.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new',
+  templateUrl: './new.component.html'
+})
+export class NewComponent implements OnInit {
+
+  constructor() { }
+
+  public name="Marvellous";
+
+  ngOnInit(): void {
+  }
+
+}
